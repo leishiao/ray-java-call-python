20c196dd94b9f21680047ca8e319638e136d3675
lei xiao <xiao.lei2893@nvxclouds.com>
2023-03-07 11:55:02 +0800

    successful example config

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-HOST_IP_ADDRESS=192.168.0.119
+HEAD_IP_ADDRESS=192.168.66.1
+HOST_IP_ADDRESS=192.168.67.1
 PYTHON_CODE_DIR=$(realpath $(pwd)/python)
 
-# -Dray.node-ip=${HOST_IP_ADDRESS}
-
 java  -Dray.job.code-search-path=${PYTHON_CODE_DIR} \
-      -Dray.address=${HOST_IP_ADDRESS}:6379 \
+      -Dray.address=${HEAD_IP_ADDRESS}:8179 \
+      -Dray.node-ip=${HOST_IP_ADDRESS} \
       -Dray.worker.mode=DRIVER \
       -jar target/call-python-0.0.1-jar-with-dependencies.jar
